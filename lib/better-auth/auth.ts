f9120756889959f140f386520cdedb997bc38cfc
chayan-1906 <chayan19062000@gmail.com>
2025-11-20 23:04:09 +0530

    feat: 🔐 integrate Better-Auth with MongoDB for email/password authentication

---
+++ lib/better-auth/auth.ts
@@ -0,0 +1,39 @@
+import {betterAuth} from "better-auth";
+import {nextCookies} from "better-auth/next-js";
+import {mongodbAdapter} from "better-auth/adapters/mongodb";
+import {connectToDatabase} from "@/database/mongoose";
+import {BETTER_AUTH_SECRET, BETTER_AUTH_URL} from "@/lib/config";
+
+let authInstance: ReturnType<typeof betterAuth> | null = null;
+
+const getAuth = async () => {
+	if (authInstance) return authInstance;
+
+	const mongoose = await connectToDatabase();
+	const db = mongoose.connection.db;
+
+	if (!db) {
+		throw new Error('MongoDB connection not found')
+	}
+
+	authInstance = betterAuth({
+		database: mongodbAdapter(db as any),
+		secret: BETTER_AUTH_SECRET,
+		baseURL: BETTER_AUTH_URL,
+		emailAndPassword: {
+			enabled: true,
+			disableSignUp: false,
+			requireEmailVerification: false,
+			minPasswordLength: 8,
+			maxPasswordLength: 128,
+			autoSignIn: true,
+		},
+		plugins: [
+			nextCookies(),
+		],
+	});
+
+	return authInstance;
+}
+
+export {getAuth};
--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,4 @@
 export const NODE_ENV = process.env.NODE_ENV;
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET;
+export const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL;
